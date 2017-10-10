24479a85fbc824eeb8ef64463de89a252a526906
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-10 12:46:34 -0700

    appending to fill stairs

--- Hacker Rank/Algorithms/staircase.py
+++ Hacker Rank/Algorithms/staircase.py
@@ -1,12 +1,21 @@
 def staircase():
     n = 6
-    for num in range(0,n):
-        print("#" * ((n-1)-num), "o" * (num + 1))
+    # for num in range(0,n):
+    #     print("#" * ((n-1)-num), "o" * (num + 1))
+    #     # print("o" * (num + 1))
 
-    # print("#" * (n - 5))
-    # print("#" * (n - 4))
-    # print("#" * (n - 3))
-    # print("#" * (n - 2))
-    # print("#" * (n - 1))
-    # print("#" * n)
+    for num in range(0,n):
+        spread = list("0" * ((n-1)-num))
+        if len(spread) < n:
+            spread.append("#")
+        # b = spread[(n-1):] + [0]
+        print(spread)
+
+        # if num < (n-1):
+        
+        #     print("0")
+        # else:
+        #     print("#")
+
+    
 staircase()
