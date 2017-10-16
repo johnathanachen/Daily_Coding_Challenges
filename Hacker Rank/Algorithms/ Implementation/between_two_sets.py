8c264e2b72b9bebc9455e7af598687770d61628a
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-16 14:45:59 -0700

    need to solve for cases without a set number of inputs

--- Hacker Rank/Algorithms/ Implementation/between_two_sets.py
+++ Hacker Rank/Algorithms/ Implementation/between_two_sets.py
@@ -1,13 +1,34 @@
 #!/bin/python3
 import sys
 
-def getTotalX(a, b):
-    # Complete this function
+def getTotalX():
+    n,m = [2, 3]
+    a = [2, 4]
+    b = [16, 32, 96]
+    indexes = []
+    combined = a + b
+    max_int = max(combined)
+    min_int = min(combined)
+    range_list = range(min_int,max_int)
+    a_to_b = []
+    answer = []
 
-if __name__ == "__main__":
-    n, m = input().strip().split(' ')
-    n, m = [int(n), int(m)]
-    a = list(map(int, input().strip().split(' ')))
-    b = list(map(int, input().strip().split(' ')))
-    total = getTotalX(a, b)
-    print(total)
+    for i in range(min_int,max_int):
+        if i % a[0] == 0 and i % a[1] == 0:
+            print(i)
+            if b[0] % i == 0 and b[1] % i == 0 and b[2] % i == 0:
+                answer.append(i)
+ 
+    # for i in range_list:
+    #     if i % ai == 0:
+    #         print(i)
+
+    # for i in set(a_to_b):
+    #     for bi in b:
+    #         if bi % i == 0:
+    #             answer.append(i)
+
+    # print(answer)
+
+
+getTotalX()
