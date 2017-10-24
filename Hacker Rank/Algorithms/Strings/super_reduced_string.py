e02a3b0762bfc887c8cbc98cb80947c26aeb3deb
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-24 13:29:50 -0700

    reduce strings

--- Hacker Rank/Algorithms/Strings/super_reduced_string.py
+++ Hacker Rank/Algorithms/Strings/super_reduced_string.py
@@ -1,5 +1,26 @@
-def super_reduced_string():
-    s = "aaabccddd"
+def should_continue(s):
+    for index in range(len(s)):
+        if index == 0:
+            previous_letter = s[index]
+        else:
+            if s[index] == previous_letter:
+                return True
+            previous_letter = s[index]
+    return False
+
+def super_reduced_string(s):
+    previous_letter = ""
+    while should_continue(s)
+        for index in range(len(s)):
+            temp = s
+            if index == 0:
+                previous_letter = s[index]
+            else:
+                if s[index] = previous_letter:
+                    s = temp[:index-1] + temp[index + 1:]
+                    break
+                else: 
+                    previous_letter = s[index]
 
 
-super_reduced_string()
+super_reduced_string("aaabccddd")
