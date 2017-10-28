0d495aaf799e4e994b02fb1e83fcad44c687a29e
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-28 14:16:36 -0700

    find string solution

--- Hacker Rank/Python Track/Strings/Find_a_string.py
+++ Hacker Rank/Python Track/Strings/Find_a_string.py
@@ -1,12 +1,20 @@
-def count_substring(string, sub_string):
-    string = string.upper()
-    sub_string = sub_string.upper()
-    counter = 0
+# def count_substring(string, sub_string):
+#     string = string.upper()
+#     sub_string = sub_string.upper()
+#     counter = 0
+#
+#     # for i in range(0, len(string)):
+#     #     if string.find(sub_string):
+#     #         counter += 1
+#     print(string.find(sub_string))
 
-    # for i in range(0, len(string)):
-    #     if string.find(sub_string):
-    #         counter += 1
-    print(string.find(sub_string))
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)-len(sub_string)+1):
+        if (string[i:i+len(sub_string)] == sub_string):
+            count += 1
+    print(count)
+    return count
 
 
 count_substring("ABCDABC", "ABC")
