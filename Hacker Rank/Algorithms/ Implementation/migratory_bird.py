1285f9980b92c379d50bd558b3108d70220f8a16
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-07 07:32:53 -0800

    fix edge case

--- Hacker Rank/Algorithms/ Implementation/migratory_bird.py
+++ Hacker Rank/Algorithms/ Implementation/migratory_bird.py
@@ -1,1 +1,10 @@
 def migratoryBirds(n, ar):
+    count = []
+    for i in ar:
+        count.append(ar.count(i))
+    max_num = max(count)
+    index_of_num = count.index(max_num)
+    highest_ID = ar[index_of_num]
+
+
+migratoryBirds(6, [1,4,4,4,5,3])
