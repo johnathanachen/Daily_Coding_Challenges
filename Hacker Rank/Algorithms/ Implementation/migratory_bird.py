4aae0ad178e0a4646e38acff5d72d20fbdb9f01b
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-07 07:48:55 -0800

    pass

--- Hacker Rank/Algorithms/ Implementation/migratory_bird.py
+++ Hacker Rank/Algorithms/ Implementation/migratory_bird.py
@@ -1,10 +1,27 @@
 def migratoryBirds(n, ar):
     count = []
+    arry_of_highest_num_index = []
+    biggest = []
+
     for i in ar:
         count.append(ar.count(i))
+
     max_num = max(count)
-    index_of_num = count.index(max_num)
-    highest_ID = ar[index_of_num]
+
+    for index, i in enumerate(count):
+        if i == max_num:
+            arry_of_highest_num_index.append(index)
+
+    # get a list of the most frequent ID
+    for index, i in enumerate(ar):
+        for x in arry_of_highest_num_index:
+            if index == x:
+                biggest.append(i)
+
+    # find the smallest ID from the most occured list
+    one_set = list(set(biggest))
+    smallest_most_freq = min(one_set)
+    print(smallest_most_freq)
 
 
-migratoryBirds(6, [1,4,4,4,5,3])
+migratoryBirds(6, [1,4,4,4,5,5,5])
