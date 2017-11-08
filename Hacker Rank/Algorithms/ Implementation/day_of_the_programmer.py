1b5d0bd666b8b219317682edd966ca6b06001c5e
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-08 08:08:12 -0800

    second step

--- Hacker Rank/Algorithms/ Implementation/day_of_the_programmer.py
+++ Hacker Rank/Algorithms/ Implementation/day_of_the_programmer.py
@@ -1,4 +1,19 @@
-def solve(year):
-    # Complete this function
+def find_year(year):
+    if year in range(1700, 1918):
+        calendar_system = {"Gregorian": year}
+    elif year in range(1918, 2701):
+        calendar_system = {"Julian": year}
+    return calendar_system
 
-solve(2017)
+def check_leap_year(calendar_system):
+    calendar_name = ''.join(calendar_system.keys())
+    if calendar_name == "Gregorian":
+        print("yes")
+
+
+
+def main():
+    calendar_system = find_year(1704)
+    check_leap_year(calendar_system)
+
+start = main()
