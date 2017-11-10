0d2605b2d1565a0e33efc297640b02cc580224d9
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-10 07:29:17 -0800

    day of programmer

--- Hacker Rank/Algorithms/ Implementation/day_of_the_programmer.py
+++ Hacker Rank/Algorithms/ Implementation/day_of_the_programmer.py
@@ -1,42 +1,58 @@
-def find_calender_system(year):
-    """ Given calendar year,finds the calendar system to the year """
-    if year in range(1700, 1918):
-        calendar_system = {"Gregorian": year}
-    elif year in range(1918, 2701):
-        calendar_system = {"Julian": year}
-    return calendar_system
-
-def check_leap_year(calendar_system):
-    """ Given the year of the calendar_system, check if the year is a leap year """
-    calendar_name = ''.join(calendar_system.keys())
-    if calendar_name == "Gregorian":
-        calender_year = calendar_system["Gregorian"]
-        if calender_year % 400 == 0:
-            if calender_year % 4 == 0 and calender_year % 100 != 0:
-                return True
-            else:
-                return False
-    elif calendar_name == "Julian":
-        calender_year = calendar_system["Julian"]
-        if calender_year % 4 == 0:
-            return True
-        else:
-            return False
-
-def print_256th_day(is_leap_year):
-    # normal_years = [31,28,31,30,31,30,31,31]
-    # leap_year = [31,29,31,30,31,30,31,31]
-    if is_leap_year:
-        print("12.09."+str(year))
+def solve(year):
+    if (year == 1918):
+        return '26.09.1918'
+    elif ((year <= 1917) & (year%4 == 0)) or ((year > 1918) & (year%400 == 0 or ((year%4 == 0) & (year%100 != 0)))):
+        return '12.09.%s' %year
     else:
-        print("13.09."+str(year))
+        return '13.09.%s' %year
 
-year = 2017
+
 def main():
     """ Start Program """
-    calendar_system = find_calender_system(year)
-    is_leap_year = check_leap_year(calendar_system)
-    print_256th_day(is_leap_year)
+    year = int(input().strip())
+    result = solve(year)
+    print(result)
+
+
+# def find_calender_system(year):
+#     """ Given calendar year,finds the calendar system to the year """
+#     if year in range(1700, 1918):
+#         calendar_system = {"Gregorian": year}
+#     elif year in range(1918, 2701):
+#         calendar_system = {"Julian": year}
+#     return calendar_system
+#
+# def check_leap_year(calendar_system):
+#     """ Given the year of the calendar_system, check if the year is a leap year """
+#     calendar_name = ''.join(calendar_system.keys())
+#     if calendar_name == "Gregorian":
+#         calender_year = calendar_system["Gregorian"]
+#         if calender_year % 400 == 0:
+#             if calender_year % 4 == 0 and calender_year % 100 != 0:
+#                 return True
+#             else:
+#                 return False
+#     elif calendar_name == "Julian":
+#         calender_year = calendar_system["Julian"]
+#         if calender_year % 4 == 0:
+#             return True
+#         else:
+#             return False
+#
+# def print_256th_day(is_leap_year,year):
+#     # normal_years = [31,28,31,30,31,30,31,31]
+#     # leap_year = [31,29,31,30,31,30,31,31]
+#     if is_leap_year:
+#         print("12.09."+str(year))
+#     else:
+#         print("13.09."+str(year))
+#
+# year = 2017
+# def main():
+#     """ Start Program """
+#     calendar_system = find_calender_system(year)
+#     is_leap_year = check_leap_year(calendar_system)
+#     print_256th_day(is_leap_year,year)
 
 
 start = main()
